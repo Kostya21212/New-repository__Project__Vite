8665a6f1b829b6b981c71cefe1171d2998901c99
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-01-22 16:35:48 +0200

    Change

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions:{
             input:{
                 main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, './pages/about.html')
+                about: resolve(__dirname, 'pages/about.html')
             },
         },
     },
